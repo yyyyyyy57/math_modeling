2937cb4f468c2f47e97bb840b5c24803d568b28e
yyyyyyy57 <63540084+yyyyyyy57@users.noreply.github.com>
2025-02-26 16:40:04 +0000

    estb

--- task_2.py
+++ task_2.py
@@ -12,7 +12,8 @@ def move_func(s, t):
      x2, v_x2, y2, v_y2,
      x3, v_x3, y3, v_y3,
      x4, v_x4, y4, v_y4,
-     x5, v_x5, y5, v_y5) = s
+     x5, v_x5, y5, v_y5,
+     x6, v_x6, y6, v_y6) = s
     
     dxdt1 = v_x1
     dv_xdt1 = k*q1*Q * x1 / (x1**2 + y1**2)**1.5 / m1
@@ -38,12 +39,18 @@ def move_func(s, t):
     dv_xdt5 = k*q5*Q * x5 / (x5**2 + y5**2)**1.5 / m5
     dydt5 = v_y5
     dv_ydt5 = k*q5*Q * y5 / (x5**2 + y5**2)**1.5 / m5
+
+    dxdt6 = v_x6
+    dv_xdt6 = k*q6*Q * x6 / (x6**2 + y6**2)**1.5 / m6
+    dydt6 = v_y5
+    dv_ydt6 = k*q6*Q * y6 / (x6**2 + y6**2)**1.5 / m6
     
     return (dxdt1, dv_xdt1, dydt1, dv_ydt1,
             dxdt2, dv_xdt2, dydt2, dv_ydt2,
             dxdt3, dv_xdt3, dydt3, dv_ydt3,
             dxdt4, dv_xdt4, dydt4, dv_ydt4,
-            dxdt5, dv_xdt5, dydt5, dv_ydt5)
+            dxdt5, dv_xdt5, dydt5, dv_ydt5,
+            dxdt6, dv_xdt6, dydt6, dv_ydt6)
 
 k = 9 * 10**9
 Q = 0.001
@@ -65,7 +72,7 @@ m2 = 0.5
 x03 = -30
 v_x03 = -10
 y03 = -10
-v_y03 = 15
+v_y03 = -10
 q3 = -0.0001
 m3 = 0.1
 
@@ -77,17 +84,25 @@ q4 = -0.0001
 m4 = 0.8
 
 x05 = -30
-v_x05 = -10
+v_x05 = 20
 y05 = 30
-v_y05 = 15
+v_y05 = -5
 q5 = -0.0001
 m5 = 0.8
+
+x06 = -30
+v_x06 = 20
+y06 = -20
+v_y06 = -5
+q6 = 0.0001
+m6 = 0.3
 
 s0 = (x01, v_x01, y01, v_y01,
       x02, v_x02, y02, v_y02,
       x03, v_x03, y03, v_y03,
       x04, v_x04, y04, v_y04,
-      x05, v_x05, y05, v_y05)
+      x05, v_x05, y05, v_y05,
+      x06, v_x06, y06, v_y06)
 sol = odeint(move_func, s0, t)
 
 fig, ax = plt.subplots()
@@ -106,6 +121,9 @@ ball_line4, = plt.plot([], [], '-', color='b')
 
 ball5, = plt.plot([], [], 'o', color='b')
 ball_line5, = plt.plot([], [], '-', color='b')
+
+ball6, = plt.plot([], [], 'o', color='b')
+ball_line6, = plt.plot([], [], '-', color='b')
 
 plt.plot([0], [0], 'o', color='y', ms=20)
 
@@ -130,6 +148,9 @@ def animate(i):
 
     ball5.set_data([sol[i][16]], [sol[i][18]])
     ball_line5.set_data(sol[:i, 16], sol[:i, 18])
+
+    ball5.set_data([sol[i][20]], [sol[i][22]])
+    ball_line5.set_data(sol[:i, 20], sol[:i, 22])
 
 ani = FuncAnimation(fig, animate, frames=frames, interval=30)
 

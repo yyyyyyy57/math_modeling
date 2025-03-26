7c0fd675107a6aeb1dedbc1ef8819755371b284a
yyyyyyy57 <63540084+yyyyyyy57@users.noreply.github.com>
2025-03-26 12:16:48 +0000

    1ttawg

--- DW_3.py
+++ DW_3.py
@@ -7,33 +7,21 @@ temp_opt = 295.65
 albedo_barren = 0.5
 albedo_white = 0.75
 albedo_black = 0.25
+albedo_3 = 0.5
 area_white = 0.01
 area_black = 0.01
+area_3 = 0.01
 death_rate = 0.3
 S = 1000
 sigma = 5.67032e-8
 insul = 0.12
-
-R_1 = 0.42 * 6.957*10**8            # глизе 667 Сс
-T_1 = 3700
-a_1 = 0.12 * 149597870700
-
-R_2 = 0.1967 * 6.957*10**8            # росс 128 b
-T_2 = 3306
-a_2 = 0.0496 * 149597870700
-
-def find_s(R, T, a):
-    S = R**2 * sigma * T**4 / a**2
-    return S
-
-S = find_s(R_2, T_2, a_2)
-
 
 if __name__ == '__main__':
 
     luminosity = np.arange(0.5, 1.6, 0.0001)
     area_black_a = np.zeros_like(luminosity)
     area_white_a = np.zeros_like(luminosity)
+    area_3_a = np.zeros_like(luminosity)
     temp_planet_a = np.zeros_like(luminosity)
     
     for j, lum in enumerate(luminosity):
@@ -42,12 +30,15 @@ if __name__ == '__main__':
             area_black = 0.01
         if area_white < 0.01:
             area_white = 0.01
-        area_barren = 1 - (area_black + area_white)
+        if area_3 < 0.01:
+            area_3 = 0.01
+        area_barren = 1 - (area_black + area_white + area_3)
 
-        albedo_planet = (area_black * albedo_black + area_white * albedo_white + area_barren * albedo_barren)
-        temp_planet = (lum*S*(1-albedo_planet)/sigma/4)**(0.25)
-        temp_black = (insul*lum*S/sigma/4*(albedo_planet-albedo_black) + temp_planet**4)**(0.25)
-        temp_white = (insul*lum*S/sigma/4*(albedo_planet-albedo_white) + temp_planet**4)**(0.25)
+        albedo_planet = (area_black * albedo_black + area_white * albedo_white +  area_3 * albedo_3 + area_barren * albedo_barren)
+        temp_planet = (lum*S*(1-albedo_planet)/sigma)**(0.25)
+        temp_black = (insul*lum*S/sigma*(albedo_planet-albedo_black) + temp_planet**4)**(0.25)
+        temp_white = (insul*lum*S/sigma*(albedo_planet-albedo_white) + temp_planet**4)**(0.25)
+        temp_3 = (insul*lum*S/sigma*(albedo_planet-albedo_3) + temp_planet**4)**(0.25)
 
         if (temp_black >= temp_min and temp_black <= temp_max):
            birth_black = 1 - 0.003265*(temp_opt-temp_black)**2
@@ -56,21 +47,28 @@ if __name__ == '__main__':
         if (temp_white >= temp_min and temp_white <= temp_max):
             birth_white = 1 - 0.003265*(temp_opt-temp_white)**2
         else:
-            birth_white = 0.0
+            birth_white = 0
+        if (temp_3 >= temp_min and temp_3 <= temp_max):
+            birth_3 = 1 - 0.003265*(temp_opt-temp_3)**2
+        else:
+            birth_3 = 0
 
         area_black += area_black*(birth_black*area_barren-death_rate)
         area_white += area_white*(birth_white*area_barren-death_rate)
+        area_3 += area_3*(birth_3*area_barren-death_rate)
 
         area_black_a[j] = area_black
         area_white_a[j] = area_white
+        area_3_a[j] = area_3
         temp_planet_a[j] = temp_planet
 
     fig, ax = plt.subplots(2, 1)
     ax[0].plot(luminosity, 100*area_black_a, color='black')
     ax[0].plot(luminosity, 100*area_white_a, color='red')
+    ax[0].plot(luminosity, 100*area_3_a, color='green')
     ax[0].set_ylabel('Area (%)')
 
     ax[1].plot(luminosity, temp_planet_a-273.15, color='black')
     ax[1].set_xlabel('Solar luminosity')
     ax[1].set_ylabel('Global temperature (°C)')
-    plt.savefig('DW.png')
+    plt.savefig('DW_3.png')
--- DW_4.py
+++ DW_4.py
@@ -5,10 +5,14 @@ temp_min = 278.15
 temp_max = 313.15
 temp_opt = 295.65
 albedo_barren = 0.5
-albedo_white = 0.75
-albedo_black = 0.25
+albedo_white = 0.8
+albedo_black = 0.2
+albedo_3 = 0.4
+albedo_4 = 0.6
 area_white = 0.01
 area_black = 0.01
+area_3 = 0.01
+area_4 = 0.01
 death_rate = 0.3
 S = 1000
 sigma = 5.67032e-8
@@ -16,9 +20,11 @@ insul = 0.12
 
 if __name__ == '__main__':
 
-    luminosity = np.arange(0.5, 1.6, 0.0001)
+    luminosity = np.arange(0.5, 1.7, 0.0001)
     area_black_a = np.zeros_like(luminosity)
     area_white_a = np.zeros_like(luminosity)
+    area_3_a = np.zeros_like(luminosity)
+    area_4_a = np.zeros_like(luminosity)
     temp_planet_a = np.zeros_like(luminosity)
     
     for j, lum in enumerate(luminosity):
@@ -27,12 +33,22 @@ if __name__ == '__main__':
             area_black = 0.01
         if area_white < 0.01:
             area_white = 0.01
-        area_barren = 1 - (area_black + area_white)
+        if area_3 < 0.01:
+            area_3 = 0.01
+        if area_4 < 0.01:
+            area_4 = 0.01
+        area_barren = 1 - (area_black + area_white + area_3 + area_4)
 
-        albedo_planet = (area_black * albedo_black + area_white * albedo_white + area_barren * albedo_barren)
+        albedo_planet = (area_black * albedo_black + 
+                         area_white * albedo_white + 
+                         area_3 * albedo_3 + 
+                         area_4 * albedo_4 + 
+                         area_barren * albedo_barren)
         temp_planet = (lum*S*(1-albedo_planet)/sigma)**(0.25)
         temp_black = (insul*lum*S/sigma*(albedo_planet-albedo_black) + temp_planet**4)**(0.25)
         temp_white = (insul*lum*S/sigma*(albedo_planet-albedo_white) + temp_planet**4)**(0.25)
+        temp_3 = (insul*lum*S/sigma*(albedo_planet-albedo_3) + temp_planet**4)**(0.25)
+        temp_4 = (insul*lum*S/sigma*(albedo_planet-albedo_4) + temp_planet**4)**(0.25)
 
         if (temp_black >= temp_min and temp_black <= temp_max):
            birth_black = 1 - 0.003265*(temp_opt-temp_black)**2
@@ -42,20 +58,34 @@ if __name__ == '__main__':
             birth_white = 1 - 0.003265*(temp_opt-temp_white)**2
         else:
             birth_white = 0
+        if (temp_3 >= temp_min and temp_3 <= temp_max):
+            birth_3 = 1 - 0.003265*(temp_opt-temp_3)**2
+        else:
+            birth_3 = 0
+        if (temp_4 >= temp_min and temp_4 <= temp_max):
+            birth_4 = 1 - 0.003265*(temp_opt-temp_4)**2
+        else:
+            birth_4 = 0
 
         area_black += area_black*(birth_black*area_barren-death_rate)
         area_white += area_white*(birth_white*area_barren-death_rate)
+        area_3 += area_3*(birth_3*area_barren-death_rate)
+        area_4 += area_4*(birth_4*area_barren-death_rate)
 
         area_black_a[j] = area_black
         area_white_a[j] = area_white
+        area_3_a[j] = area_3
+        area_4_a[j] = area_4
         temp_planet_a[j] = temp_planet
 
     fig, ax = plt.subplots(2, 1)
     ax[0].plot(luminosity, 100*area_black_a, color='black')
     ax[0].plot(luminosity, 100*area_white_a, color='red')
+    ax[0].plot(luminosity, 100*area_3_a, color='green')
+    ax[0].plot(luminosity, 100*area_4_a, color='blue')
     ax[0].set_ylabel('Area (%)')
 
     ax[1].plot(luminosity, temp_planet_a-273.15, color='black')
     ax[1].set_xlabel('Solar luminosity')
     ax[1].set_ylabel('Global temperature (°C)')
-    plt.savefig('DaisyWorld.png')
+    plt.savefig('DW_4.png')
